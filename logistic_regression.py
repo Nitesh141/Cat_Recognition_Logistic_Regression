0bc2aefc453ed62ceb03f45765c5b8e9b06612c3
Nitesh Kumar <nk14@iitbbs.ac.in>
2018-03-22 11:10:47 +0530

    Add files via upload

---
+++ logistic_regression.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+from lr_module import *
+
+
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+
+
+m_train = len(train_set_y.T)
+m_test = len(test_set_y.T)
+num_px = len(train_set_x_orig[0])
+
+# print ("Number of training examples: m_train = " + str(m_train))
+# print ("Number of testing examples: m_test = " + str(m_test))
+# print ("Height/Width of each image: num_px = " + str(num_px))
+# print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+# print ("train_set_x shape: " + str(train_set_x_orig.shape))
+# print ("train_set_y shape: " + str(train_set_y.shape))
+# print ("test_set_x shape: " + str(test_set_x_orig.shape))
+# print ("test_set_y shape: " + str(test_set_y.shape))
+
+
+
+# Reshape the training and test examples
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+#
+# print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+# print ("train_set_y shape: " + str(train_set_y.shape))
+# print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+# print ("test_set_y shape: " + str(test_set_y.shape))
+# print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
+
+
+
+print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
+
+
+
+
+
+
+dim = 2
+w, b = initialize_with_zeros(dim)
+print ("w = " + str(w))
+print ("b = " + str(b))
+
+
+w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
+
+
+
+
+
+params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+
+print ("w = " + str(params["w"]))
+print ("b = " + str(params["b"]))
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+
+
+
+
+
+w = np.array([[0.1124579],[0.23106775]])
+b = -0.3
+X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+print ("predictions = " + str(predict(w, b, X)))
+
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+
+# Plot learning curve (with costs)
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
